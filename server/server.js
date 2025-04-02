5bde16dbae636defde57e90cb16d485802f938d9
UsainSrht <usainsrht@gmail.com>
2025-04-03 01:21:24 +0300

    add files

---
+++ server/server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+const rooms = {}; // Store room data
+const players = {};
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("submit-name", (name) => {
+    console.log(`${name} joined. (${socket.id})`);
+    players[socket.id] = {
+        name: name,
+        created: Date.now(),
+        current_room: null
+    };
+  });
+
+  socket.on("create-room", (roomCode) => {
+    rooms[roomCode] = { 
+        players: [socket.id], kicked_players: [], 
+        started: false, created: Date.now()
+    };
+    socket.join(roomCode);
+    console.log(`${getName(socket.id)}(${socket.id}) created room: ${roomCode}`);
+  });
+
+  socket.on("join-room", (roomCode) => {
+    if (rooms[roomCode]) {
+      socket.join(roomCode);
+      rooms[roomCode].players.push(socket.id);
+      console.log(`User ${getName(socket.id)}(${socket.id}) joined room ${roomCode}`);
+      io.to(roomCode).emit("player-joined", rooms[roomCode].players);
+    } else {
+      socket.emit("error", "Room not found");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${getName(socket.id)}(${socket.id})`);
+    if (socket.id in players && players[socket.id].current_room !== null) {
+        let room = players[socket.id].current_room;
+        let name = players[socket.id].name;
+        io.to(room).emit("player-left", name);
+    }
+  });
+});
+
+server.listen(3000, () => console.log("Server running on port 3000"));
+
+function getName(socketId) {
+    return players[socketId].name;
+}
