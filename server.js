1655310097bce0cc96b09613a5fdc28346d7dc85
UsainSrht <usainsrht@gmail.com>
2025-04-06 18:27:25 +0300

    enhance lobby and game selection features with improved UI and socket handling

--- server.js
+++ server.js
@@ -49,6 +49,14 @@ io.on("connection", (socket) => {
   socket.on("submit-name", (receivedName, isInGame) => {
     const userId = getUserId(socket.id);
     console.log(`User submitted name: ${receivedName}(${userId})(${socket.id})`);
+    const currentRoom = getRoom(userId);
+    if (currentRoom && rooms[currentRoom]) {
+      if (rooms[currentRoom].players.map(getName).includes(receivedName)) {
+        socket.emit("error", "Name already taken in this room");
+        socket.emit("request-user-id"); // ask for name again
+        return;
+      }
+    }
     players[userId] = players[userId] || {}; 
     players[userId].name = receivedName;
     players[userId].socketId = socket.id;
@@ -59,7 +67,8 @@ io.on("connection", (socket) => {
     const userId = getUserId(socket.id);
     rooms[roomCode] = { 
         players: [], kicked_players: [], 
-        started: false, created: Date.now()
+        started: false, created: Date.now(),
+        host: userId, game: null
     };
     socket.join(roomCode);
     console.log(`${getName(userId)}(${socket.id}) created room: ${roomCode}`);
@@ -86,11 +95,11 @@ io.on("connection", (socket) => {
         console.log(`${userId} added to room ${roomCode}`);
         rooms[roomCode].players.push(userId);
       }
-      io.to(roomCode).emit("player-list", rooms[roomCode].players.map(getName));
+      io.to(roomCode).emit("player-list", rooms[roomCode].players.map(userId => ({ [getName(userId)]: userId })));
       socket.join(roomCode);
       io.to(roomCode).emit("player-joined", name);
       players[userId].current_room = roomCode;
-      socket.emit("send-room-data", roomCode, rooms[roomCode].players.map(getName), rooms[roomCode].started);
+      socket.emit("send-room-data", getRoomData(roomCode));
       console.log(`User ${name}(${socket.id}) joined room ${roomCode}`);
     } else {
       socket.emit("error", "Room not found");
@@ -98,8 +107,9 @@ io.on("connection", (socket) => {
   });
 
   socket.on("request-room-data", (roomCode) => {
+    //todo send detailed room data
     console.log(`sending room data ${roomCode} ${rooms[roomCode].players.length}`);
-    socket.emit("send-room-data", roomCode, rooms[roomCode].players.map(getName), rooms[roomCode].started);
+    socket.emit("send-room-data", getRoomData(roomCode));
   });
 
   socket.on("chat-message", (message) => {
@@ -108,6 +118,21 @@ io.on("connection", (socket) => {
     if (room) {
       console.log(`${room}: ${getName(userId)}: ${message}`);
       io.to(room).emit("chat-message", message, getName(userId));
+    } else {
+      socket.emit("error", "You are not in a room");
+    }
+  });
+
+  socket.on("select-game", (game) => {
+    const userId = getUserId(socket.id);
+    const room = getRoom(userId);
+    if (room && rooms[room]) {
+      if (rooms[room].host !== userId) {
+        socket.emit("error", "You are not the host of this room");
+        return;
+      }
+      rooms[room].game = game;
+      io.to(room).emit("game-selected", game);
     } else {
       socket.emit("error", "You are not in a room");
     }
@@ -122,7 +147,7 @@ io.on("connection", (socket) => {
         if (room in rooms) {
           io.to(room).emit("player-left", name);
           rooms[room].players = rooms[room].players.filter(player => player !== userId);
-          io.to(room).emit("player-list", rooms[room].players.map(getName));
+          io.to(room).emit("player-list", rooms[roomCode].players.map(userId => ({ [getName(userId)]: userId })));
         }
     }
     //add timeout and delete
@@ -143,4 +168,8 @@ function getUserId(socketId) {
 
 function getRoom(userId) {
   return userId in players ? ("current_room" in players[userId] ? players[userId].current_room : null) : null;
+}
+
+function getRoomData(roomCode) {
+  return roomCode, rooms[roomCode].players.map(userId => ({ [getName(userId)]: userId })), rooms[roomCode].host, rooms[roomCode].started, rooms[roomCode].game;
 }
