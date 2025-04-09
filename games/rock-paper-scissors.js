b57e608de3f544ef048096b1ee3e9c471923b8ca
UsainSrht <usainsrht@gmail.com>
2025-04-09 08:27:13 +0300

    add logging for successful game script loading

--- games/rock-paper-scissors.js
+++ games/rock-paper-scissors.js
@@ -1,23 +1,30 @@
 
-module.exports = (io, room, roomData, players) => {
-  console.log(`Initializing rock paper scissors for room: ${room}`);
-
-  io.to(room).emit("rps-game-started");
-
-  io.in(room).on("connection", (socket) => {
-
-    socket.on("rps-select-move", (move) => {
-      const userId = getUserId(socket.id);
-      if (!userId) return;
-      io.emit("rps-move-selected", move, userId);
-    });
-
-  });
-
-  function getUserId(socketId) {
-    return Object.entries(players).find(([key, user]) => user.socketId === socketId)?.[0];
+(function () {
+  if (typeof module !== "undefined" && module.exports) {
+    //server
+    module.exports = (io, room, roomData, players) => {
+      console.log(`Initializing rock paper scissors for room: ${room}`);
+    
+      io.to(room).emit("rps-game-started");
+    
+      io.in(room).on("connection", (socket) => {
+    
+        socket.on("rps-select-move", (move) => {
+          const userId = getUserId(socket.id);
+          if (!userId) return;
+          io.emit("rps-move-selected", move, userId);
+        });
+    
+      });
+    
+      function getUserId(socketId) {
+        return Object.entries(players).find(([key, user]) => user.socketId === socketId)?.[0];
+      }
+    
+    };
+  } else {
+    //client
+    alert("Rock Paper Scissors game started! Select your move: rock, paper, or scissors.");
   }
-
-};
-
-alert("Rock Paper Scissors game started! Select your move: rock, paper, or scissors.");
+  
+})();
