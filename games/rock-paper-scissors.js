d15995b277d430c1f89e585642133f4aa44ffc13
UsainSrht <usainsrht@gmail.com>
2025-04-11 09:27:46 +0300

    rps game page and remove light mode styles

--- games/rock-paper-scissors.js
+++ games/rock-paper-scissors.js
@@ -1,7 +1,11 @@
 module.exports = (io, room, roomData, players) => {
   console.log(`Initializing rock paper scissors for room: ${room}`);
 
-  io.to(room).emit("rps-game-started");
+  const countdown = 7;
+
+  io.to(room).emit("rps-game-started", countdown);
+
+  const playerMoves = {};
 
   io.on("connection", (socket) => {
 
@@ -11,10 +15,22 @@ module.exports = (io, room, roomData, players) => {
     socket.on("rps-select-move", (move) => {
       const userId = getUserId(socket.id);
       if (!userId) return;
-      io.to(room).emit("rps-move-selected", move, userId);
+      io.to(room).emit("rps-move-selected", move);
+    });
+
+    socket.on("rps-countdown-end", () => {
+
     });
 
   });
+
+  setTimeout(() => {
+    countdownEnd();
+  }, countdown * 1000);
+
+  function countdownEnd() {
+
+  }
 
   //returns only for players that are in this room
   function getUserId(socketId) {
