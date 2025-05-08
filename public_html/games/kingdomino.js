b4628f636c817d88488166fd04b52ad64bbdf0c7
UsainSrht <usainsrht@gmail.com>
2025-05-08 08:32:02 +0300

    relocate methods

--- public_html/games/kingdomino.js
+++ public_html/games/kingdomino.js
@@ -46,115 +46,7 @@ class KingdominoScene extends Phaser.Scene {
     
         socket.emit("kingdomino-preload-finish");
     }
-    
-    update() {
-        // game loop
-    }
-    
-    create() {
-        socket.emit("kingdomino-create-start");
-    
-        const gameBoard = document.getElementById('game-board');
-    
-        const loadingLabel = document.createElement("div");
-        loadingLabel.id = "loading-label";
-        loadingLabel.className = "flex items-center justify-center h-screen w-screen bg-gray-800 bg-opacity-50 fixed top-0 left-0 z-50";
-        loadingLabel.innerHTML = `<svg aria-hidden="true" class="w-8 h-8 text-gray-600 animate-spin fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                                    </svg>`;
-        gameBoard.appendChild(loadingLabel);
-    
-        this.backgroundRect = this.add.rectangle(
-            0,
-            0,
-            this.scale.width,
-            this.scale.height
-        )
-        .setOrigin(0)
-        .setStrokeStyle(1, 0xffffff);
-    
-        this.add.text(5, 5, 'Kingdomino', { fontSize: '32px', color: '#ffffff' });
-    
-        // Create groups
-        this.freeTiles = this.add.group();    // tiles you can pick
-        this.placedTiles = this.add.group();  // tiles locked on the grid
-    
-        // Player grid
-        this.mainGrid = drawPlayerGrid(this, config.width/2, config.height-300, 'YOU', 5, 100, this.placedTiles);
-    
-        // Other players (no interactivity needed)
-        this.secondGrid = drawPlayerGrid(this, config.width/2, 110, 'Player 2', 5, 30);
-        this.thirdGrid = drawPlayerGrid(this, 180, config.height/2, 'Player 3', 5, 30);
-        this.fourthGrid = drawPlayerGrid(this, config.width-180, config.height/2, 'Player 4', 5, 30);
-    
-        socket.on("kingdomino-game-start", (tiles) => {
-            gameBoard.removeChild(loadingLabel);
-            createTestTiles(this, tiles);
-        });
-    
-        // Input listeners
-        this.input.on('pointerdown', (pointer) => {
-            if (pointer.rightButtonDown()) {
-                if (this.grabbedTile) {
-                    this.grabbedTile.angle += 90;
-                }
-            } else {
-                if (!this.grabbedTile) {
-                    const tiles = this.freeTiles.getChildren();
-                    for (let i = tiles.length - 1; i >= 0; i--) { // Topmost first
-                        const tile = tiles[i];
-                        if (tile.getBounds().contains(pointer.x, pointer.y)) {
-                            console.log('Picked up free tile');
-                            this.grabbedTile = tile;
-                            this.children.bringToTop(tile);
-                            break;
-                        }
-                    }
-                }
-            }
-        });
-    
-        this.input.on('pointermove', (pointer) => {
-            if (this.grabbedTile) {
-                this.grabbedTile.x = pointer.x;
-                this.grabbedTile.y = pointer.y;
-            }
-        });
-    
-        this.input.on('pointerup', (pointer) => {
-            if (this.grabbedTile) {
-                const gridSize = 5;
-                const tileSize = this.scale.width / 10; // dynamically calculated
-                const centerX = this.scale.width / 2;
-                const centerY = this.scale.height - this.scale.height / 4;
-        
-                if (
-                    isInsideGrid(this.grabbedTile, centerX, centerY, gridSize, tileSize) &&
-                    canPlaceTile(this.grabbedTile, this.placedTiles, centerX, centerY, gridSize, tileSize)
-                ) {
-                    console.log('Placed tile into grid');
-                    this.grabbedTile.disableInteractive(); // Lock it
-                    this.freeTiles.remove(this.grabbedTile);
-                    this.placedTiles.add(this.grabbedTile);
-        
-                    snapTileToGrid(this.grabbedTile, centerX, centerY, gridSize, tileSize);
-                }
-                this.grabbedTile = null;
-            }
-        });
-    
-        socket.emit("kingdomino-create-finish");
-    }
-    
-    resize(width, height) {
-        this.backgroundRect.setSize(width, height);
-        this.mainGrid.setSize(width, height);
-        this.secondGrid.setSize(width, height);
-        this.thirdGrid.setSize(width, height);
-        this.fourthGrid.setSize(width, height);
-    }
-    
+
     drawPlayerGrid(scene, centerX, centerY, playerName, gridSize, tileSize, placedGroup = null) {
         const container = scene.add.container(); // Acts like a group
         const graphics = scene.add.graphics({ lineStyle: { width: 1, color: 0xffffff } });
@@ -322,6 +214,113 @@ class KingdominoScene extends Phaser.Scene {
         tile.y = gridStartY + row * tileSize;
     }
     
+    update() {
+        // game loop
+    }
+    
+    create() {
+        socket.emit("kingdomino-create-start");
+    
+        const gameBoard = document.getElementById('game-board');
+    
+        const loadingLabel = document.createElement("div");
+        loadingLabel.id = "loading-label";
+        loadingLabel.className = "flex items-center justify-center h-screen w-screen bg-gray-800 bg-opacity-50 fixed top-0 left-0 z-50";
+        loadingLabel.innerHTML = `<svg aria-hidden="true" class="w-8 h-8 text-gray-600 animate-spin fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                    </svg>`;
+        gameBoard.appendChild(loadingLabel);
+    
+        this.backgroundRect = this.add.rectangle(
+            0,
+            0,
+            this.scale.width,
+            this.scale.height
+        )
+        .setOrigin(0)
+        .setStrokeStyle(1, 0xffffff);
+    
+        this.add.text(5, 5, 'Kingdomino', { fontSize: '32px', color: '#ffffff' });
+    
+        // Create groups
+        this.freeTiles = this.add.group();    // tiles you can pick
+        this.placedTiles = this.add.group();  // tiles locked on the grid
+    
+        // Player grid
+        this.mainGrid = drawPlayerGrid(this, config.width/2, config.height-300, 'YOU', 5, 100, this.placedTiles);
+    
+        // Other players (no interactivity needed)
+        this.secondGrid = drawPlayerGrid(this, config.width/2, 110, 'Player 2', 5, 30);
+        this.thirdGrid = drawPlayerGrid(this, 180, config.height/2, 'Player 3', 5, 30);
+        this.fourthGrid = drawPlayerGrid(this, config.width-180, config.height/2, 'Player 4', 5, 30);
+    
+        socket.on("kingdomino-game-start", (tiles) => {
+            gameBoard.removeChild(loadingLabel);
+            createTestTiles(this, tiles);
+        });
+    
+        // Input listeners
+        this.input.on('pointerdown', (pointer) => {
+            if (pointer.rightButtonDown()) {
+                if (this.grabbedTile) {
+                    this.grabbedTile.angle += 90;
+                }
+            } else {
+                if (!this.grabbedTile) {
+                    const tiles = this.freeTiles.getChildren();
+                    for (let i = tiles.length - 1; i >= 0; i--) { // Topmost first
+                        const tile = tiles[i];
+                        if (tile.getBounds().contains(pointer.x, pointer.y)) {
+                            console.log('Picked up free tile');
+                            this.grabbedTile = tile;
+                            this.children.bringToTop(tile);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    
+        this.input.on('pointermove', (pointer) => {
+            if (this.grabbedTile) {
+                this.grabbedTile.x = pointer.x;
+                this.grabbedTile.y = pointer.y;
+            }
+        });
+    
+        this.input.on('pointerup', (pointer) => {
+            if (this.grabbedTile) {
+                const gridSize = 5;
+                const tileSize = this.scale.width / 10; // dynamically calculated
+                const centerX = this.scale.width / 2;
+                const centerY = this.scale.height - this.scale.height / 4;
+        
+                if (
+                    isInsideGrid(this.grabbedTile, centerX, centerY, gridSize, tileSize) &&
+                    canPlaceTile(this.grabbedTile, this.placedTiles, centerX, centerY, gridSize, tileSize)
+                ) {
+                    console.log('Placed tile into grid');
+                    this.grabbedTile.disableInteractive(); // Lock it
+                    this.freeTiles.remove(this.grabbedTile);
+                    this.placedTiles.add(this.grabbedTile);
+        
+                    snapTileToGrid(this.grabbedTile, centerX, centerY, gridSize, tileSize);
+                }
+                this.grabbedTile = null;
+            }
+        });
+    
+        socket.emit("kingdomino-create-finish");
+    }
+    
+    resize(width, height) {
+        this.backgroundRect.setSize(width, height);
+        this.mainGrid.setSize(width, height);
+        this.secondGrid.setSize(width, height);
+        this.thirdGrid.setSize(width, height);
+        this.fourthGrid.setSize(width, height);
+    }
 }
 
 const config = {
